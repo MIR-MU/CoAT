c53a034e72e71440522b78242b9cc68ec298c707
stefanik12 <stefanik.mich@gmail.com>
2023-01-14 21:13:57 +0100

    SGlue Tasks: __str__ for caching

--- evaluation/tasks/en/superglue.py
+++ evaluation/tasks/en/superglue.py
@@ -25,6 +25,7 @@ class SuperGLUE(Task, abc.ABC):
         super().__init__()
         template = DatasetTemplates(self.promptsource_id)
         self.prompt = template[prompts_template]
+        self.label = prompts_template
 
         if hf_dataset_identifiers is not None:
             from datasets import load_dataset
